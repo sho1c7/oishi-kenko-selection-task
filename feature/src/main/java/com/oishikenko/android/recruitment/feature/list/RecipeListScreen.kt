b4839e78eae304f6c906b5fdc9808fcc8f78cd2c
sho1c7 <sho.1c7@icloud.com>
2022-11-29 01:28:10 +0900

    #1 RecipeListScreenのヘッダーを変更（実機未確認、画像ありで未確認）

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
@@ -1,20 +1,22 @@
 package com.oishikenko.android.recruitment.feature.list
 
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.consumedWindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -27,25 +29,41 @@ fun RecipeListScreen(
 ) {
     val cookingRecords by viewModel.cookingRecords.collectAsStateWithLifecycle()
     Scaffold(
-        topBar = {
-            TopAppBar(
-                modifier = Modifier,
-            ) {
-                Text(text = stringResource(id = R.string.cooking_records_title))
+        content = { innerPadding ->
+            Column {
+                Row (
+                    modifier = Modifier
+                        .padding(top = 8.dp)
+                        .fillMaxWidth()
+                        .wrapContentHeight(align = Alignment.CenterVertically),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                    ) {
+                    Text(
+                        text = stringResource(id = R.string.cooking_records_title),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.header_image),
+                        contentDescription = "ヘッダー画像",
+                        modifier = Modifier.size(width = 66.dp, height = 64.dp)
+                    )
+                }
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(innerPadding)
+                        .consumedWindowInsets(innerPadding)
+                ) {
+                    items(cookingRecords) {
+                        RecipeListItem(it)
+                    }
+                }
             }
         }
-    ) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(innerPadding)
-                .consumedWindowInsets(innerPadding)
-        ) {
-            items(cookingRecords) {
-                RecipeListItem(it)
-            }
-        }
-    }
+    )
 }
 
 @Preview
