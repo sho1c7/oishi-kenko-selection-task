c29f9e88dd4dffbc2a1a9c1b3529d5ee80a8ac20
sho1c7 <sho.1c7@icloud.com>
2022-11-29 01:27:04 +0900

    デフォルトフォントをNotosansに変更

--- app/src/main/java/com/oishikenko/android/recruitment/ui/theme/Type.kt
+++ app/src/main/java/com/oishikenko/android/recruitment/ui/theme/Type.kt
@@ -2,14 +2,26 @@ package com.oishikenko.android.recruitment.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.oishikenko.android.recruitment.R
 
 // Set of Material typography styles to start with
+
+val Notosans = FontFamily(
+        Font(R.font.noto_sans_jp_black, FontWeight.Black),
+        Font(R.font.noto_sans_jp_light, FontWeight.Light),
+        Font(R.font.noto_sans_jp_bold, FontWeight.Bold),
+        Font(R.font.noto_sans_jp_thin, FontWeight.Thin),
+        Font(R.font.noto_sans_jp_medium, FontWeight.Medium),
+        Font(R.font.noto_sans_jp_regular, FontWeight.Normal),
+)
+
 val Typography = Typography(
         body1 = TextStyle(
-                fontFamily = FontFamily.Default,
+                fontFamily = Notosans,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp
         )
