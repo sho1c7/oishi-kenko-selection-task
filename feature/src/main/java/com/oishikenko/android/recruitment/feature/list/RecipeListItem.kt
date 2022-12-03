f0d5f2cb1d3f61c42f606fbd5f1f1510edf46a3f
sho1c7 <sho.1c7@icloud.com>
2022-12-03 21:23:09 +0900

    一覧画面にレシピ種別と記録日時の表示を追加

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListItem.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListItem.kt
@@ -1,16 +1,17 @@
 package com.oishikenko.android.recruitment.feature.list
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.oishikenko.android.recruitment.data.model.CookingRecord
 
@@ -19,6 +20,7 @@ fun RecipeListItem(
     cookingRecord: CookingRecord
 ) {
     Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
             .padding(
@@ -34,6 +36,23 @@ fun RecipeListItem(
                 .size(96.dp)
                 .clip(RoundedCornerShape(4.dp)),
         )
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = cookingRecord.recipeType,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(
+                text = cookingRecord.recordedAt,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+            )
+        }
     }
 }
 
